80311c1ac38a4bb61793f40f17c502ac532a3a84
Ashley Gyngell <ashleygyngell@gmail.com>
2022-12-20 17:34:45 +0000

    added emailJS and sweetAlert for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,47 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
+import swal from 'sweetalert';
 import { Link } from 'react-router-dom';
+import { send } from 'emailjs-com';
 
 const Contact = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: 'info@bridgefamilymediation.co.uk',
+    message: '',
+    reply_to: ''
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send('service_z6diadp', 'template_mwatvbp', toSend, 'EDa-3yOSvDtlcxcpr')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        e.target.elements[0].value = '';
+        e.target.elements[1].value = '';
+        e.target.elements[2].value = '';
+        swal(
+          `Hi ${toSend.from_name}.`,
+          'Thank you for your email, we will get back to you as soon as possible'
+        );
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <div className="content mt-2   ">
         <div className="title is-4 mr-6 ml-6 ">Contact us</div>
 
         <div className=" pt-5 pl-6 pr-6">
-          <form
-            action="mailto:info@bridgefamilymediation.co.uk"
-            method="post"
-            name="EmailForm"
-            className=""
-          >
+          <form onSubmit={onSubmit}>
             <div className="field">
               <label
                 htmlFor="name"
@@ -25,10 +50,11 @@ const Contact = () => {
               <div className="control has-icons-left">
                 <input
                   type="text"
-                  name="name"
-                  id="name"
                   className="input"
+                  name="from_name"
                   placeholder="Name"
+                  value={toSend.from_name}
+                  onChange={handleChange}
                   autoFocus
                 />
                 <span className="icon is-left">
@@ -43,11 +69,12 @@ const Contact = () => {
               ></label>
               <div className="control has-icons-left">
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="text"
                   className="input"
+                  name="reply_to"
                   placeholder="Email"
+                  value={toSend.reply_to}
+                  onChange={handleChange}
                 />
                 <span className="icon is-left">
                   <i className="fa fa-envelope"></i>
@@ -60,11 +87,14 @@ const Contact = () => {
                 className="label is-size-4 has-text-weight-light"
               ></label>
               <textarea
+                type="text"
                 name="message"
                 id="message"
                 rows="5"
                 className="textarea is-medium"
                 placeholder="Please leave a brief message and your location"
+                value={toSend.message}
+                onChange={handleChange}
               ></textarea>
             </div>
             <button
